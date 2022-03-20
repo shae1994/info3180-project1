57bcac0f554e88ac5439ddcbf4b3e7a574d90ab9
shae1994 <juddyshaew@gmail.com>
2022-03-20 12:09:09 -0500

    Completed Assignment.

--- app/forms.py
+++ app/forms.py
@@ -12,10 +12,10 @@ from flask_wtf.file import FileRequired
 class CreateProperty(FlaskForm):
     title = StringField('Property Title', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
-    no_of_bedrooms = StringField('No. of Rooms', validators=[DataRequired()])
+    no_of_bedrooms = StringField('No. of Bedrooms', validators=[DataRequired()])
     no_of_bathrooms = StringField('No. of Bathrooms', validators=[DataRequired()])
     prices = StringField('Price', validators=[DataRequired()])
-    types = SelectField('Property Types', choices=[('Single-Family', 'Single-Family'), ('Multi-Family', 'Multi-Family'), ('Apartment', 'Apartment'), ('Town-House', 'Town-House'), ('Mansion', 'Mansion'), ('Villa', 'Villa'), ('Condo', 'Condo'), ('Co-operative', 'Co-operative')],validators=[InputRequired()])
+    types = SelectField('Property Types', choices=[('Single-Family House', 'Single-Family House'), ('Multi-Family House', 'Multi-Family House'), ('Apartment', 'Apartment'), ('Town-House', 'Town-House'), ('Mansion', 'Mansion'), ('Villa', 'Villa'), ('Condo', 'Condo'), ('Co-operative', 'Co-operative')],validators=[InputRequired()])
     option = SelectField('Tenure Type', choices=[('Rent', 'Rent'), ('Sale', 'Sale')],validators=[InputRequired()])
     location = StringField('Location', validators=[DataRequired()])
     photo=FileField(validators=[FileRequired(), FileAllowed(['png', 'jpg', 'jpeg','PNG', 'JPEG', 'JPG', 'jng', 'JNG', 'gif', 'GIF'])])
